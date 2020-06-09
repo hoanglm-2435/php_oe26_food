e738b9775191c67dbdcfd235c427870fcc6e5e6d
Hoanguet <hoangminhhuet@gmail.com>
2020-06-09 15:09:34 +0700

    Preparing database

--- database/migrations/2020_06_09_070008_create_products_table.php
+++ database/migrations/2020_06_09_070008_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('address');
-            $table->string('phone');
-            $table->integer('role_id')->unsigned();
-            $table->rememberToken();
+            $table->text('description');
+            $table->integer('size_id')->unsigned();
+            $table->integer('image_id')->unsigned();
+            $table->integer('quantity');
+            $table->integer('category_id');
+            $table->float('price');
+            $table->float('price_sale');
             $table->timestamps();
         });
     }
@@ -34,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 }
