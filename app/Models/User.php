b64f91a822f5cb23d41c78a66ea00f6d783fc3cf
Hoanguet <hoangminhhuet@gmail.com>
2020-06-12 14:25:47 +0700

    Login, logout for admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,7 +16,12 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'address',
+        'phone',
+        'role_id',
     ];
 
     /**
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::firstOrCreate([
+            'email' => 'admin@example.com'
+        ], [
+            'name' => 'Admin',
+            'password' => bcrypt('admin'),
+            'address' => 'Hà Nội',
+            'phone' => '0123456789',
+            'role_id' => 1,
+        ]);
+    }
+}
