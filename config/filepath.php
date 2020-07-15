ef62fd5f935d87d1939fde9df02ec206487e8d51
Hoanguet <hoangminhhuet@gmail.com>
2020-07-15 08:39:45 +0700

    Show product to Homepage

--- config/filepath.php
+++ config/filepath.php
@@ -2,4 +2,20 @@
 
 return [
     'img_product_path' => 'img/products/',
+    'logo-web' => 'img/logo.png',
+    'logo-fudink' => 'img/logo_fudink.png',
+    'footer-logo' => 'img/products/footer-logo.png',
+    'slider-1' => 'img/slider/slider-1.jpg',
+    'slider-2' => 'img/slider/slider-2.jpg',
+    'banner-1' => 'img/banner/banner-7.jpg',
+    'banner-2' => 'img/banner/banner-8.jpg',
+    'banner-3' => 'img/banner/banner-5.jpg',
+    'banner-4' => 'img/banner/banner-6.jpg',
+    'banner-5' => 'img/banner/banner-4.jpg',
+    'banner-6' => 'img/banner/banner-49.jpg',
+    'brand-logo-1' => 'img/brand-logo/logo-1.png',
+    'brand-logo-2' => 'img/brand-logo/logo-2.png',
+    'brand-logo-3' => 'img/brand-logo/logo-3.png',
+    'brand-logo-4' => 'img/brand-logo/logo-4.png',
+    'brand-logo-5' => 'img/brand-logo/logo-5.png'
 ];
